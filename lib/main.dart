33876524d37e900a67de0a57cb60f441bd3d95fa
Hubert Dudziński <hdudzinski.kontakt@gmail.com>
2025-01-14 20:37:49 +0100

    Changed to work with ConfigProvider and pass configuration to the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,57 @@
-import 'dart:io';
-
+import 'package:finance_manager/core/ColorsGen.dart';
 import 'package:finance_manager/core/themes/themes.dart';
 import 'package:finance_manager/features/presentation/home.dart';
+import 'package:finance_manager/features/state/app_config.dart';
 import 'package:flutter/material.dart';
+
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'features/data/app_database.dart';
+import 'features/provider/config_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
+  final config = await AppConfig.loadConfig();
 
-  runApp(MyApp());
+  runApp(MyApp(config: config));
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AppConfig config;
+
+  const MyApp({super.key, required this.config});
 
   @override
   Widget build(BuildContext context) {
-
     return MultiProvider(
       providers: [
         Provider(
           create: (context) => AppDatabase(),
           dispose: (context, AppDatabase db) => db.close(),
         ),
+        ChangeNotifierProvider(
+          create: (_) => ConfigProvider(config),
+        ),
       ],
-
-      child: MaterialApp(
-        title: 'Finance Manager',
-        theme: lightTheme,
-        darkTheme: darkTheme,
-        themeMode: ThemeMode.system,
-        home: MyHomePage(),
+      child: Consumer<ConfigProvider>(
+        builder: (context, configProvider, _) {
+          return MaterialApp(
+            title: 'Finance Manager',
+            theme: getLightTheme(colorMap[configProvider.selectedColor] ?? Colors.blue),
+            darkTheme: getDarkTheme(colorMap[configProvider.selectedColor] ?? Colors.blue),
+            themeMode: configProvider.systemTheme
+                ? ThemeMode.system
+                : (configProvider.darkTheme ? ThemeMode.dark : ThemeMode.light),
+            home: MyHomePage(),
+          );
+        },
       ),
     );
   }
 }
-
-
-
-
-
-
 
 
